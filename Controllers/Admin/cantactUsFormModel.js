4d6329307dc3327ad48e4b1ade9cd03b2d68c44f
Ankush Gupta <ankushgupta9675@gmail.com>
2024-08-28 12:57:54 +0530

    change conatct us get api for admin

--- Controllers/Admin/cantactUsFormModel.js
+++ Controllers/Admin/cantactUsFormModel.js
@@ -1,40 +1,47 @@
 const db = require("../../Models");
-const { contactUsForm } = require("../../Middlewares/validate")
+const { contactUsForm } = require("../../Middlewares/validate");
 const ContactUsForm = db.contactUsForm;
 
 exports.createContactUsForm = async (req, res) => {
-    try {
-        // Validate body
-        const { error } = contactUsForm(req.body);
-        if (error) {
-            console.log(error);
-            return res.status(400).json(error.details[0].message);
-        }
-        await ContactUsForm.create(req.body);
-        res.status(200).json({
-            success: true,
-            message: "Contact us form created successfully!"
-        });
-    } catch (err) {
-        res.status(500).json({
-            success: false,
-            message: err.message
-        });
+  try {
+    // Validate body
+    const { error } = contactUsForm(req.body);
+    if (error) {
+      console.log(error);
+      return res.status(400).json(error.details[0].message);
     }
-}
+    await ContactUsForm.create(req.body);
+    res.status(200).json({
+      success: true,
+      message: "Contact us form created successfully!",
+    });
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      message: err.message,
+    });
+  }
+};
 
 exports.getAllContactUsForm = async (req, res) => {
-    try {
-        const contactUs = await ContactUsForm.findAll();
-        res.status(200).json({
-            success: true,
-            message: "Contact us form fetched successfully!",
-            data: contactUs
-        });
-    } catch (err) {
-        res.status(500).json({
-            success: false,
-            message: err.message
-        });
+  try {
+    const { isMutual, others } = req.query;
+    let query = {};
+    if (isMutual) {
+      query = { where: { data_from_page: "Mutual Divorce" } };
+    } else if (others) {
+      query = { where: { data_from_page: "Others" } };
     }
-}
+    const contactUs = await ContactUsForm.findAll(query);
+    res.status(200).json({
+      success: true,
+      message: "Contact us form fetched successfully!",
+      data: contactUs,
+    });
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      message: err.message,
+    });
+  }
+};
