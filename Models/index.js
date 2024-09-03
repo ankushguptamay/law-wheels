a9fda06c57c55b78e875cd47c8899a33428e3b7d
Ankush Gupta <ankushgupta9675@gmail.com>
2024-09-03 14:07:01 +0530

    add slug in contactUsForm and add pagination in get contact us

--- Models/index.js
+++ Models/index.js
@@ -41,16 +41,16 @@ db.user.hasMany(db.mutualDivorceForm, {
 });
 db.mutualDivorceForm.belongsTo(db.user, { foreignKey: "userId", as: "user" });
 
-queryInterface
-  .addColumn("contactUsForms", "slug", {
-    type: DataTypes.STRING,
-    unique: true,
-  })
-  .then((res) => {
-    console.log("added!");
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+// queryInterface
+//   .addColumn("contactUsForms", "slug", {
+//     type: DataTypes.STRING,
+//     unique: true,
+//   })
+//   .then((res) => {
+//     console.log("added!");
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
 
 module.exports = db;
