0549ec6df95ce015c2306bbf9193567e7aa77184
Ankush Gupta <ankushgupta9675@gmail.com>
2024-09-25 18:12:57 +0530

    isMobilrVerified of contact us

--- Models/index.js
+++ Models/index.js
@@ -157,20 +157,10 @@ db.contactUsForm.hasMany(db.contactUsLeadLogs, {
 //     console.log(err);
 //   });
 
-queryInterface
-  .addColumn("contactUsForms", "isMobileVerified", {
-    type: DataTypes.BOOLEAN,
-    defaultValue: false,
-  })
-  .then((res) => {
-    console.log("added1!");
-  })
-  .catch((err) => {
-    console.log(err);
-  });
 // queryInterface
-//   .addColumn("blogs", "description", {
-//     type: DataTypes.TEXT,
+//   .addColumn("contactUsForms", "isMobileVerified", {
+//     type: DataTypes.BOOLEAN,
+//     defaultValue: false,
 //   })
 //   .then((res) => {
 //     console.log("added1!");
