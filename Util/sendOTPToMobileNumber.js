b9e10bd8640f8ce734ff802a08614d7d855f90cc
Ankush Gupta <ankushgupta9675@gmail.com>
2025-01-30 18:29:34 +0530

    otp template change

--- Util/sendOTPToMobileNumber.js
+++ Util/sendOTPToMobileNumber.js
@@ -1,14 +1,21 @@
-const axios = require('axios');
-const { SMS_ROUTE_ID, SMS_CHANNEL, SMS_API_KEY, SMS_SENDER_ID, SMS_DLT_ENTITY_ID, SMS_DLT_TEMPLATE_ID } = process.env;
+const axios = require("axios");
+const {
+  SMS_ROUTE_ID,
+  SMS_CHANNEL,
+  SMS_API_KEY,
+  SMS_SENDER_ID,
+  SMS_DLT_ENTITY_ID,
+  SMS_DLT_TEMPLATE_ID,
+} = process.env;
 
 exports.sendOTP = async (phoneNumber, otp) => {
-    try {
-        const message = `Enter OTP ${otp} to verify you number at Law Wheels. - Law Wheels`;
-        let response = await axios.post(
-            `https://www.smsgatewayhub.com/api/mt/SendSMS?APIKey=${SMS_API_KEY}&senderid=${SMS_SENDER_ID}&channel=${SMS_CHANNEL}&DCS=0&flashsms=0&number=${phoneNumber}&text=${message}&route=${SMS_ROUTE_ID}&EntityId=${SMS_DLT_ENTITY_ID}&dlttemplateid=${SMS_DLT_TEMPLATE_ID}`
-        );
-        return response;
-    } catch (e) {
-        console.log('Something went wrong in sending SMS: ', e);
-    }
-}
+  try {
+    const message = `${otp} is your login OTP for Law Wheels App. Do not share it with anyone.`;
+    let response = await axios.post(
+      `https://www.smsgatewayhub.com/api/mt/SendSMS?APIKey=${SMS_API_KEY}&senderid=${SMS_SENDER_ID}&channel=${SMS_CHANNEL}&DCS=0&flashsms=0&number=${phoneNumber}&text=${message}&route=${SMS_ROUTE_ID}&EntityId=${SMS_DLT_ENTITY_ID}&dlttemplateid=${SMS_DLT_TEMPLATE_ID}`
+    );
+    return response;
+  } catch (e) {
+    console.log("Something went wrong in sending SMS: ", e);
+  }
+};
